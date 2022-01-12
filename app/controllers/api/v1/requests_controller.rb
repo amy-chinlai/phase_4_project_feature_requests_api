661ea1bf23eec9965260dea1241fe833727716f7
amy-chinlai <amy@actionnetwork.org>
2022-01-12 16:34:57 -0500

    Create method and route

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::RequestsController < ApplicationController
         requests = Request.all
         render json: requests, include: [:category]
     end
+
+    def create
+        request = Request.new(request_params)
+        if request.save
+            render json: request, status: :accepted
+        else
+            render json: {errors: request.errors.full_messages}. status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def request_params
+        params.require(request).permit(:name, :description, :category_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
-      resources :requests, only: [:index]
+      resources :requests, only: [:index, :create]
     end
   end
 
-  # get '/api/v1/requests' => 'requests#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
