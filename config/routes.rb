cbcf7430f3237243e204cb2b1d88ccbbe1469271
amy-chinlai <amy@actionnetwork.org>
2022-01-12 16:24:06 -0500

    added namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get '/' => 'requests#index'
+  namespace :api do 
+    namespace :v1 do
+      resources :requests, only: [:index]
+    end
+  end
+
+  # get '/api/v1/requests' => 'requests#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
