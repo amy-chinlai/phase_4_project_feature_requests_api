0b5a997857069e3b7a85ce0c12eb248d08ef5714
amy-chinlai <amy@actionnetwork.org>
2022-01-14 10:26:15 -0500

    vote as column on requests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_204349) do
+ActiveRecord::Schema.define(version: 2022_01_14_151917) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2022_01_12_204349) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "vote"
   end
 
 end
